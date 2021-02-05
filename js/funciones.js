f1ab2f2ca6b9182aed27ce6c737eda19c854fd11
JMMOMBA <tatomomba@gmail.com>
2021-02-06 00:06:09 +0100

    Finalizados los filtrados

--- js/funciones.js
+++ js/funciones.js
@@ -1,10 +1,56 @@
-//pintamos un elemento solo.
-function printTareas(pTareas, pSeccionTareas) {
+//Elementos a pintar
+const sectionTareas = document.querySelector('.tareas');
+const btnForm = document.querySelector('#btn-guardar');
+const tareaInput = document.querySelector('#tareaInput');
+const prioridad = document.querySelector('#prioridad');
+
+let idTarea = 2;
+
+printTareas(tareas, sectionTareas);
+
+btnForm.addEventListener('click', (event) => {
+    tareas.push({
+        titulo: tareaInput.value,
+        prioridad: prioridad.value,
+        id: idTarea++,
+    });
+    sectionTareas.innerHTML = '';
+    printTareas(tareas, sectionTareas);
+});
+
+// btnForm.addEventListener('click', getDataForm);
+
+// function getDataForm(event) {
+//     event.preventDefault();
+//     const tituloTarea = tareaInput.value;
+//     const prioridadTarea = prioridad.value;
+
+//     if (tituloTarea != "" && prioridadTarea != "") {
+//         const newTarea = {
+//             id: idTarea++,
+//             titulo: tituloTarea,
+//             prioridad: prioridadTarea
+//         };
+//         printTareas(tareas, newTarea);
+//     } else {
+//         alert("Debes rellenar todos los campos");
+            
+//     }
+// }
+
+
+
+//INTRODUCCIÓN DE TAREAS
+function printTareas(pTareas, pIntroduceTareas) {
+    
     for (let tarea of tareas) {
-        const newTarea = printUnaTarea(tarea);
-        pSeccionTareas.appendChild(newTarea);
+        const nuevaTarea = printUnaTarea(tarea);
+        pIntroduceTareas.appendChild(nuevaTarea);
+        
     }
+    
 }
+//Pintamos un elemento solo.
 function printUnaTarea(pTarea) {
     let div = document.createElement('div'); 
     switch (pTarea.prioridad) {
@@ -37,3 +83,56 @@ function printUnaTarea(pTarea) {
     
     return div;    
 }
+
+//FILTRADO DE TAREAS
+
+// function filtrarTareas (pPrioridad, pTareas) {
+//     let listaFiltrada = pTareas.filter (tarea => tarea.prioridad == pPrioridad);
+
+//     return listaFiltrada;
+// }
+
+let seleccionaPrioridad = document.querySelector('#busquedaPrioridad');
+
+seleccionaPrioridad.addEventListener('change', getBusquedaPrioridad);
+
+
+function getBusquedaPrioridad(event) {
+    let busquedaPrioridad = seleccionaPrioridad.value;
+
+    if (busquedaPrioridad == 'urgente') {
+        let listaPrioridad = listaTareas.filter(tarea => tarea.prioridad == 'urgente');
+        printTareas(listaPrioridad);
+    } else if (prioridad == 'diaria') {
+        let listaPrioridad = listaTareas.filter(tarea => tarea.prioridad == 'diaria');
+        printTareas(listaPrioridad);
+    } else if (prioridad == 'mensual') {
+        let listaPrioridad = listaTareas.filter(tarea => tarea.prioridad == 'mensual');
+        printTareas(listaPrioridad);
+    } else {
+        printTareas(listaTareas);
+    }
+}
+
+//TAREA A BUSCAR
+
+const inputTarea = document.querySelector('#busquedaInput');
+
+inputTarea.addEventListener('keydown', getInputBusqueda)
+
+function getInputBusqueda(event) {
+    let inputBusqueda = inputTarea.value;
+    if (inputBusqueda == '') {
+        printTareas(listaTareas);
+    } else {
+        let searchByWord = listaTareas.filter(tarea => tarea.titulo.toLowerCase().includes(inputBusqueda.toLowerCase()));
+        printTareas(searchByWord);
+    }   
+}
+
+// function searchByWord(pWord, pTareas) {
+//     const listaFiltrada = pTareas.filter( tarea => {
+//         return tarea.titulo.toLowerCase().includes(pWord);
+//     })
+//     return listaFiltrada;
+// }
